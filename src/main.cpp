56c3b944374dc2d3a97e80cc0586be760126319e
rkfg <rkfg@rkfg.me>
2018-11-11 16:25:14 +0300

    Suppress server exceptions (cosmetic)

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,21 @@
 #include "State.h"
 
 int main(int argc, char **argv) {
-    Wt::WServer server(argc, argv);
-    State state(server);
-    server.addEntryPoint(Wt::EntryPointType::Application, [&](const Wt::WEnvironment& env) {
-        return std::make_unique<ChatApp>(env, server, state);
-    });
-    if (server.start()) {
-        int sig = Wt::WServer::waitForShutdown();
-        Wt::log("warn") << "Shutdown (signal = " << sig << ")";
-        server.stop();
-        if (sig == SIGHUP)
-            Wt::WServer::restart(argc, argv, environ);
+    try {
+        Wt::WServer server(argc, argv);
+        State state(server);
+        server.addEntryPoint(Wt::EntryPointType::Application, [&](const Wt::WEnvironment& env) {
+            return std::make_unique<ChatApp>(env, server, state);
+        });
+        if (server.start()) {
+            int sig = Wt::WServer::waitForShutdown();
+            Wt::log("warn") << "Shutdown (signal = " << sig << ")";
+            server.stop();
+            if (sig == SIGHUP)
+                Wt::WServer::restart(argc, argv, environ);
+        }
+    } catch (const Wt::WServer::Exception& e) {
+        return 1;
     }
     return 0;
 }
